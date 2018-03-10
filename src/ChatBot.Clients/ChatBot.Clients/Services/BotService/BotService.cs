97d45466dc0ed81ada242d2df423b1d9b50bf5e1
Wilson Vargas <wilsonvargas_6@outlook.com>
2018-03-09 19:53:55 -0500

    Added BotService

---
+++ src/ChatBot.Clients/ChatBot.Clients/Services/BotService/BotService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.Clients.Services.BotService
+{
+    public class BotService : IBotService
+    {
+
+        #region Properties
+
+        private HttpClient _cliente;
+
+        public HttpClient Client
+        {
+            get { return _cliente; }
+            set { _cliente = value; }
+        }
+
+
+        #endregion
+        public Task Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<string> PostAsync(string uri, HttpContent content)
+        {
+            try
+            {
+                HttpResponseMessage response = await startConversationClient.PostAsync(uri, content);
+
+                Stream stream = await response.Content.ReadAsStreamAsync();
+                StreamReader readStream = new StreamReader(stream, Encoding.UTF8);
+                string result = readStream.ReadToEnd();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ src/ChatBot.Clients/ChatBot.Clients/Services/BotService/IBotService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.Clients.Services.BotService
+{
+    public interface IBotService
+    {
+        Task Connect();
+        Task SendMessage();
+    }
+}
